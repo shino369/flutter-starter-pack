995d4532e8f8694ceca363d44b29db2ff4e4150a
AnthonyW <nakagame02@gmail.com>
2024-07-04 16:52:09 +0900

    add dice roller

---
+++ dice_roller/lib/dice_roller.dart
@@ -0,0 +1,46 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+final randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() => _DiceRollerState();
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  int diceNum = 1;
+
+  void rollDice() {
+    setState(() {
+      diceNum = randomizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Center(
+            child: Image.asset('assets/images/dice-$diceNum.png', width: 200)),
+        const SizedBox(
+          height: 20,
+        ),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+              // padding: const EdgeInsets.only(top: 20),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(fontSize: 28)),
+          child: const Text(
+            'Roll Dice',
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ dice_roller/lib/gradient_container.dart
@@ -0,0 +1,31 @@
+import 'package:dice_roller/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+const Alignment startAlignment = Alignment.topLeft;
+const Alignment endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key, required this.colors});
+
+  const GradientContainer.purple({super.key})
+      : colors = const [Colors.deepPurple, Colors.indigo];
+
+  final List<Color> colors;
+
+
+
+  @override
+  Widget build(context) {
+    return Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: colors,
+            begin: startAlignment,
+            end: endAlignment,
+          ),
+        ),
+        child: const Center(
+          child: DiceRoller()
+        ));
+  }
+}
